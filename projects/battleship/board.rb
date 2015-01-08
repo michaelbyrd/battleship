ba40b2121f1d3db23d2444e664d107ce651de224
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-01-08 11:56:41 -0500

    issues with firing at board

--- projects/battleship/board.rb
+++ projects/battleship/board.rb
@@ -13,17 +13,34 @@ class Board
   end
 
   def has_ship_on?(str)
-    if @ships == []
-      false
+    @ships.any? do |ship|
+      ship.covers?(x_of(str), y_of(str))
+    end
+  end
+
+  def fire_at(str)
+    has_ship_on?(str)
+  end
+
+  def has_ship_coords?(x,y)
+    @ships.any? do |ship|
+      ship.covers?(x,y)
+    end
+  end
+
+  def check_ship(ship, str, across)
+    if across
+      coords = (x_of(str)...x_of(str)+ship.length).to_a.map!{|x|[x,y_of(str)]}
     else
-      @ships.any? do |ship|
-        ship.covers?(x_of(str), y_of(str))
-      end
+      coords = (y_of(str)...y_of(str)+ship.length).to_a.map!{|y|[x_of(str),y]}
+    end
+    coords.any? do |coord|
+      has_ship_coords?(coord[0],coord[1])
     end
   end
 
   def place_ship(ship, str, across)
-    unless has_ship_on?(str)
+    unless check_ship(ship, str, across)
       ship.place(x_of(str),y_of(str),across)
       @ships << ship
     else
